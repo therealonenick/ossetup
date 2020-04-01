756025111b275b81cc16197e4b0746430b659681
therealonenick <62951829+therealonenick@users.noreply.github.com>
2020-04-01 09:30:28 -0400

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
+echo "Getting Version..."
+RHVER=$(rpm -E %{rhel})
+
 #Update and install some basic items
+echo "Installing Updates and base packages."
 yum update -y && \
     yum install -y\
-    yum-plugin-fastestmirror\
     epel-release\
     open-vm-tools\
     net-tools\
@@ -13,23 +16,26 @@ yum update -y && \
     nodejs\
 
 #Install PM2
+echo "Installing PM2."
 npm install pm2
 
 
 #Elrepo Install
+echo "Importing ELRepo Key..."
 rpm --import https://www.elrepo.org/RPM-GPG-KEY-elrepo.org
 
-echo "Getting Version..."
-RHVER=$(rpm -E %{rhel})
+echo "Installing ELRepo.."
+#ELRepo recommends using yum-plugin-fastestmirror but its not availble with Centos8 as of this writing.
+#adding it only to v7 and 6.
 if [ $RHVER="8" ]
 then
     yum install -y https://www.elrepo.org/elrepo-release-8.1-1.el8.elrepo.noarch.rpm
 elif [ $RHVER="7" ]
 then
-    yum install -y https://www.elrepo.org/elrepo-release-7.0-4.el7.elrepo.noarch.rpm
+    yum install -y yum-plugin-fastestmirror && https://www.elrepo.org/elrepo-release-7.0-4.el7.elrepo.noarch.rpm
 elif [ $RHVER="6" ]
 then
-    yum install -y https://www.elrepo.org/elrepo-release-6-9.el6.elrepo.noarch.rpm
+    yum install -y yum-plugin-fastestmirror & https://www.elrepo.org/elrepo-release-6-9.el6.elrepo.noarch.rpm
 else
     echo "No matching versions.  Exiting..."
     exit 1
