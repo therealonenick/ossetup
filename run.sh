053970ed8b7a06a433f9170359315835085ec42b
therealonenick <62951829+therealonenick@users.noreply.github.com>
2020-04-01 09:21:08 -0400

    Update run.sh

--- run.sh
+++ run.sh
@@ -23,13 +23,13 @@ echo "Getting Version..."
 RHVER=$(rpm -E %{rhel})
 if [ $RHVER="8" ]
 then
-    yum install https://www.elrepo.org/elrepo-release-8.1-1.el8.elrepo.noarch.rpm
+    yum install -y https://www.elrepo.org/elrepo-release-8.1-1.el8.elrepo.noarch.rpm
 elif [ $RHVER="7" ]
 then
-    yum install https://www.elrepo.org/elrepo-release-7.0-4.el7.elrepo.noarch.rpm
+    yum install -y https://www.elrepo.org/elrepo-release-7.0-4.el7.elrepo.noarch.rpm
 elif [ $RHVER="6" ]
 then
-    yum install https://www.elrepo.org/elrepo-release-6-9.el6.elrepo.noarch.rpm
+    yum install -y https://www.elrepo.org/elrepo-release-6-9.el6.elrepo.noarch.rpm
 else
     echo "No matching versions.  Exiting..."
     exit 1
