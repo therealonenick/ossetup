d5e507fdd6f8f411aa7326856cc6d26681e2772e
therealonenick <62951829+therealonenick@users.noreply.github.com>
2020-04-01 09:17:48 -0400

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,14 +1,39 @@
 #!/bin/bash
 
+#Update and install some basic items
 yum update -y && \
     yum install -y\
+    yum-plugin-fastestmirror\
+    epel-release\
     open-vm-tools\
     net-tools\
     git\
     policycoreutils\
     setools\
-    htop\
-    ntop\
     nodejs\
 
+#Install PM2
 npm install pm2
+
+
+#Elrepo Install
+rpm --import https://www.elrepo.org/RPM-GPG-KEY-elrepo.org
+
+echo "Getting Version..."
+RHVER=$(rpm -E %{rhel})
+if [ $RHVER="8" ]
+then
+    yum install https://www.elrepo.org/elrepo-release-8.1-1.el8.elrepo.noarch.rpm
+elif [ $RHVER="7" ]
+then
+    yum install https://www.elrepo.org/elrepo-release-7.0-4.el7.elrepo.noarch.rpm
+elif [ $RHVER="6" ]
+then
+    yum install https://www.elrepo.org/elrepo-release-6-9.el6.elrepo.noarch.rpm
+else
+    echo "No matching versions.  Exiting..."
+    exit 1
+fi
+
+
+
